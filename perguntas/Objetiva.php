466a8573ffe45539e405aad37ce5371e52e27c10
CaiosRamos <139660671+buvho@users.noreply.github.com>
2025-09-22 10:46:22 -0300

    primeira versão

---
+++ perguntas/Objetiva.php
@@ -0,0 +1,39 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $i = (int)tratar_dado($_POST["linha"]);
+
+        $file = fopen("pergunta.txt",'r');
+
+        $pergunta = $file[$i];
+        $valores = explode(";",$pergunta);
+        $titulo = $valores[0];
+    }
+    function tratar_dado($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+    }  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body style="margin: 20px">
+    <h1>Responda a pergunta:</h1>
+    <h3><?php echo($pergunta); ?></h3>
+    <div class="col-md-6">
+        <form action="responderObjetiva.php" method="POST">
+        <div class="form-group">
+            <label>Resposta</label>
+            <input type="text" class="form-control" name="resposta" placeholder="Resposta">
+        </div>
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+</body>
+</html>
